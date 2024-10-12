c1ee7977da79682472de9147b069ad6c02113cdc
YamaguchiAtsushi <yamaguchi.atsushi2000@gmail.com>
2024-10-12 15:33:56 +0900

    change code

--- src/waypoint_publisher.cpp
+++ src/waypoint_publisher.cpp
@@ -10,6 +10,15 @@
 #include <vector>
 #include <cmath>
 
+#define SET_MAX_SPEED_FULL 0
+#define SET_MAX_SPEED_HALF 1
+#define STOP 2
+#define SKIP_WAYPOINT 3
+#define PUBLISH_SUB_WAYPOINT 4
+#define PUBLISH_NEXT_WAYPOINT 5
+
+int state_ = PUBLISH_NEXT_WAYPOINT;
+
 class WaypointPublisher {
 private:
     ros::NodeHandle nh_;
@@ -18,28 +27,30 @@ private:
     ros::Publisher marker_pub_;
     ros::Publisher waypoint_pub_;
     ros::Subscriber goal_reached_sub_; // goal_reachedをサブスクライブするためのSubscriber
+    ros::Publisher goal_reached_pub_;
+
 
     geometry_msgs::Twist twist_;
     std::vector<geometry_msgs::PoseStamped> waypoints_;
     int waypoint_index_;
-    bool goal_reached_;
+    bool goal_reached_ =true;
     double robot_x_, robot_y_, yaw_, odom_robot_x_, odom_robot_y_;
 
 public:
     WaypointPublisher() : waypoint_index_(0), goal_reached_(false) {
-        odom_sub_ = nh_.subscribe("odom", 1000, &WaypointPublisher::odomCallback, this);
-        twist_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+        odom_sub_ = nh_.subscribe("/ypspur_ros/odom", 1000, &WaypointPublisher::odomCallback, this);
+        twist_pub_ = nh_.advertise<geometry_msgs::Twist>("/ypspur_ros/cmd_vel", 1000);
         marker_pub_ = nh_.advertise<visualization_msgs::Marker>("visualization_marker", 1);
         waypoint_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("next_waypoint", 1);
 
-        // goal_reached_をサブスクライブ
+        goal_reached_pub_ = nh_.advertise<std_msgs::Bool>("goal_reached", 10);
         goal_reached_sub_ = nh_.subscribe("goal_reached", 1000, &WaypointPublisher::goalReachedCallback, this);
     }
 
     // goal_reachedを受け取るコールバック関数
     void goalReachedCallback(const std_msgs::Bool::ConstPtr& msg) {
         goal_reached_ = msg->data; // サブスクライブした値でgoal_reached_を更新
-        ROS_INFO("goal_reached_ = %s", goal_reached_ ? "true" : "false");
+        // ROS_INFO("goal_reached_ = %s", goal_reached_ ? "true" : "false");
     }
 
     void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
@@ -103,18 +114,71 @@ public:
         marker_pub_.publish(marker);
     }
 
-    void update() {
-        if (waypoint_index_ < waypoints_.size()) {
-            geometry_msgs::PoseStamped current_goal = waypoints_[waypoint_index_];
+    void publishWaypoint() {
 
-            // 現在のゴールを次のウェイポイントとしてpublish
-            waypoint_pub_.publish(current_goal);
 
-            if (goal_reached_) {
-                goal_reached_ = false;
-                waypoint_index_++;
-            }
+        switch (state_) {
+            case SET_MAX_SPEED_FULL:
+                
+                break;
+
+            case PUBLISH_NEXT_WAYPOINT:
+                if (waypoint_index_ < waypoints_.size()) {
+                    // if (goal_reached_) {
+                    //     goal_reached_ = false;
+                    //     waypoint_index_+= 1;
+                    // }
+
+                if (goal_reached_) {
+                    goal_reached_ = false;
+                    waypoint_index_ += 1;
+
+                    // ウェイポイントのパブリッシュ
+                    geometry_msgs::PoseStamped current_goal = waypoints_[waypoint_index_];
+                    waypoint_pub_.publish(current_goal);
+                }
+                    // std::cout << "goal_reached" << goal_reached_ << std::endl;
+
+                    std_msgs::Bool goal_reached_msg;
+                    goal_reached_msg.data = goal_reached_;
+                    goal_reached_pub_.publish(goal_reached_msg);
+                    std::cout << "wayponint_index_:" << waypoint_index_ << std::endl;
+
+                    geometry_msgs::PoseStamped current_goal = waypoints_[waypoint_index_];
+
+                    // 現在のゴールを次のウェイポイントとしてpublish
+                    waypoint_pub_.publish(current_goal);
+
+                    // std::cout << "goal_reached_:" << goal_reached_ << std::endl;
+
+                    // 現在のゴールを次のウェイポイントとしてpublish
+                    waypoint_pub_.publish(current_goal);
+                }
+                break;
+            
+            default:
+                break;
         }
+
+        // if (waypoint_index_ < waypoints_.size()) {
+        //     if (goal_reached_) {
+        //         goal_reached_ = false;
+        //         waypoint_index_+= 1;
+        //     }
+        //     std::cout << "wayponint_index_:" << waypoint_index_ << std::endl;
+
+        //     geometry_msgs::PoseStamped current_goal = waypoints_[waypoint_index_];
+
+        //     // 現在のゴールを次のウェイポイントとしてpublish
+        //     waypoint_pub_.publish(current_goal);
+
+        //     // std::cout << "goal_reached_:" << goal_reached_ << std::endl;
+
+        //     // 現在のゴールを次のウェイポイントとしてpublish
+        //     waypoint_pub_.publish(current_goal);
+
+        // }
+
 
         twist_pub_.publish(twist_);
         publishWaypointsMarker();
@@ -123,7 +187,7 @@ public:
     void run() {
         ros::Rate loop_rate(10);
         while (ros::ok()) {
-            update();
+            publishWaypoint();
             ros::spinOnce();
             loop_rate.sleep();
         }
@@ -131,10 +195,12 @@ public:
 };
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "waypoint_follower");
+    ros::init(argc, argv, "waypoint_publisher");
 
     WaypointPublisher wp;
-    std::string csv_file = "/path/to/your/waypoints.csv";
+    //std::string csv_file = "../csv/waypoints.csv";
+    std::string csv_file = "/home/yamaguchi-a/catkin_ws/src/waypoint_publisher/csv/waypoints.csv";
+
 
     if (!wp.readWaypointsFromCSV(csv_file)) {
         return 1;
